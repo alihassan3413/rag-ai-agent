a6ba6f60796eb29587f1de205f39bb553dd62c9e
alihassan3413 <alihassan113413@gmail.com>
2025-12-13 01:40:46 +0500

    update: middleware added to allow apis request

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from api.routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Insurance Agent RAG Bot")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or put your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 
